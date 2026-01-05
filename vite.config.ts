20ff3b13add71d929abae3625723da6cf1d0c035
Your Name <2097726768@qq.com>
2026-01-06 05:25:41 +0800

    fix: import defineConfig

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
-// vite.config.ts
+import { defineConfig } from 'vite' // 👈 之前就是少了这一行！
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
     port: 5173,
-    allowedHosts: true, // 或者写 ['ltb-design.zeabur.app']
+    allowedHosts: true, // 允许 Zeabur 域名访问
   }
 })
